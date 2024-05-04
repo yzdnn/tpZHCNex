1bf676d674f73db9e71bff9bb04b3f2884f47cef
ROCO2021 <1401548627@qq.com>
2024-05-04 22:27:39 +0800

    更新库下载源。

--- repo.js
+++ repo.js
@@ -6,7 +6,7 @@
 		"tsa": "Used to make the STG game display the '♪' normally."
 	},
 	"servers": [
-		"https://mirrors.thpatch.rcopky.top/tpZHCNex/",
+		"hhttps://mirrors.lilywhite.cc/tpZHCNex/",
 		"https://mirrors.thpatch.net/tpZHCNex/",
 		"https://raw.githubusercontent.com/Yanstory/tpZHCNex/master/"
 	],
